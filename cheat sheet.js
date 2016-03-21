503e98dc96ac6ddd07c53658705fbc29091144d9
david szyszkowski <try2murkme@gmail.com>
2016-03-21 14:01:29 -0700

    modified conatainer to have flipper code and tried extra divs that are off center

--- cheat sheet.js
+++ cheat sheet.js
@@ -95,3 +95,156 @@ $pablo.attr('id', 'the-ring');
 
 // test this in the console (watch the HTML, though) by running:
 keepItSecretKeepItSafe();
+
+// w02d01 tic tac toe
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col border-bottom border-right" onclick="move(0,0)"></div>
+      <div class="col border-bottom" onclick="move(0,1)"></div>
+      <div class="col border-bottom border-left" onclick="move(0,2)"></div>
+    </div>
+    <div class="row">
+      <div class="col border-right" onclick="move(1,0)"></div>
+      <div class="col" onclick="move(1,1)"></div>
+      <div class="col border-left" onclick="move(1,2)"></div>
+    </div>
+    <div class="row">
+      <div class="col border-top border-right" onclick="move(2,0)"></div>
+      <div class="col border-top" onclick="move(2,1)"></div>
+      <div class="col border-top border-left" onclick="move(2,2)"></div>
+    </div>
+
+    <div class="row">
+      <button onclick="reset()">Reset</button>
+    </div>
+  </div>
+
+  <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html
+// tic tac toe css
+.container {
+  width: 310px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.row {
+  float: left;
+  width: 100%;
+}
+
+.col {
+  float: left;
+  width: 100px;
+  height: 100px;
+  text-align: center;
+}
+
+.border-left {
+  border-left: 1px solid #000;
+}
+
+.border-right {
+  border-right: 1px solid #000;
+}
+
+.border-bottom {
+  border-bottom: 1px solid #000;
+}
+
+.border-top {
+  border-top: 1px solid #000;
+}
+
+// cd wdi/labs/jquery
+// console.log(typeof $);  // same as below
+// console.log(typeof jQuery);
+
+// var divToManipulate = document.getElementById('myDiv');
+// divToManipulate.innerHTML = 'goodbye world sad face';  // jQuery version below
+
+// $('#myDiv').html('goodbye world');  // makes 2 lines to one
+
+// $('#container').html('this isnt a container bish');
+
+
+
+// var myDiv = document.getElementById('container');
+// var newP = document.createElement('p');
+// newP.innerHTML = 'hello complicated, multi line adding dom element'; //longer
+// myDiv.appendChild(newP);
+
+// ********************************************************
+// $('#container').append('<p>');
+// $('p').html('heello simpler lines to inpute using jQuery chaining'); // shorter
+
+// $('#myBtn').on('click', function(){
+//   // $('p').addClass('red') // makes text red
+//   $('p').toggleClass('red');
+// });
+
+// $('#secondBtn').on('click', function(){
+//   event.stopPropagation()               // dont bubble up, like debugger
+//   console.log('You clicked me!!');
+// });
+
+// $('#container').on('click', function() {
+//   console.log('yessireebob!')
+// })
+
+// var containerInnerHtml = $(
+//   '#container').html()
+
+// $('#container').append('<img src="https://assets-cdn.github.com/images/modules/open_graph/github-mark.png">')
+//                                       // made image above
+// console.log(
+//   $( '#container').html())
+
+// $('#secondBtn').remove()
+
+
+// $('p').addClass('red');
+
+// $('.bold').removeClass('bold');   // lcass then remove class
+
+// var $newP = $('<p>');                  // making variable
+// $newP.html('this is another p tag');  //
+// $newP.addClass('red');                  //adding red class
+// $('#myDiv').append($newP);          //puting inisde the div
+
+
+
+
+// $('#drinks').append('<li>beer</li>')
+// $('#foods').append('<li>burgers</li>')
+// $('li').
+
+$( document ).ready(function() {
+  $('#addDrink').on('click', function(){
+    var newDrink = $('#drinkInput').val();
+    $('#drinks').append('<li>' + newDrink + '</li>');
+    $('#drinkInput').val('');
+  });
+
+  $('#addFood').on('click', function(){
+    var newFood = $('#foodInput').val();
+    $('#food').append('<li' + newFood + '</li>');
+    $('#foodInput').val(' ');    //clears
+  });
+
+  $('button').mouseover(function() {
+    // $(this).addClass('red')
+    $(this).toggleClass('red');
+    /* Act on the event */
+  });
+
+});
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
-var onclickd = function(){
+
+var onclick = function(){
+  // event.target.remove();             // used this event click, tweak it below, basic template
   console.log('I was clicked');
+  // if (event.target.style.textDecoration === 'line-through') {    //if else to see if strike has occured
+  //   event.target.remove();
+  // } else {event.target.style.textDecoration = 'line-through';}
 };
+
+// Math.random
+// }
+
+// jQuery for onclick
+// append the style or add style that transforms
+// front of card to .back
+// or maybe a .remove style that is followed by
+// an add style to .back
+
