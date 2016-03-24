baa5287ca85b26663240c82577fb63ba9bb4db56
david szyszkowski <try2murkme@gmail.com>
2016-03-24 11:46:00 -0700

    paired phil

--- js/app.js
+++ js/app.js
@@ -22,125 +22,90 @@
 // create a counter that counts number of sticks
 // i assume it would be inside of game loop
 
-$(document).ready(
-  function () {
-    $('.card').hide();
-    });
-
-
 var array = [];
+
+$('.card').hide();
 
 $('.card').on('click', function() {
   $(this).addClass('flip');
   var type = $(this).children()[1];
 
-
-  // console.log($(this).children()[1]);
   if ( $(type).hasClass( 'bomb' )) {
     window.alert("You lose");
     return reset();
-  // } else if ( 'safe' || 'stick') {
+
   } else if ($(type).hasClass( 'safe' )) {
-    // $(this).toggleClass('animated zoomIn');
+
     $(this).addClass('flip');
-    // $(type).text('SAFE');
+
   } else if ($(type).hasClass( 'stick' )) {
 
     array.push('stick');
+
     if (array.length === 3) {
+      // TODO add prompt you win
       return reset();
-      }
-      }
+    }
+  }
 // end of game
 });
 
 
-//   var counter = function() {
-//     for( var i=0; i < count.length; i++) {
-//       counter=count[i];
-
-//     }
-//   };
-
-// $(this).push
-
-
-       //  (count++);
-       //  while(count < 2) {
-       //    return window.alert("You got a stick. Now stick it somewhere");
-       //    } else
-       //    if (count === 3) {
-       //    window.alert("You won. You're a nerd!!!");return reset;
-
-       // };
 // jQuery for onclick
 // append the style or add style that transforms
 // front of card to .back
 // or maybe a .remove style that is followed by
 // an add style to .back
 
-var $el = $('this.el');
-
 var Card = function (type) {
   this.type = type;
 };
-
-
-
-
 
 var start = function() {
   // window.confirm('warning...brain combustion imminent!!!');
   //http://stackoverflow.com/questions/8618570/displaying-a-loading-gif-for-5-seconds-automatically
   //stack overflow
 
-//   var show = function(){
-//   };
-//   var hide = function() {
-//   };
-//   (function(){
-//     var myDiv = document.getElementById("myDiv");
 
-//     var show = function(){
-//       myDiv.style.display = "block";
-//       setTimeout(hide, 1900);  // 5 seconds
-//     };
+  (function(){
+    var myDiv = document.getElementById("myDiv");
 
-//     var hide = function(){
-//       myDiv.style.display = "none";
-//     };
+    var show = function(){
+      myDiv.style.display = "block";
+      setTimeout(hide, 1900);  // 5 seconds
+    };
 
-// show();
-//   })();
-//   show();
-//   hide();
-// bunch of cards in itx
-$('.card').show();
-var deck = [
-  new Card('stick'),
-  new Card('stick'),
-  new Card('stick'),
-  new Card('bomb'),
-  new Card('bomb'),
-  new Card('bomb'),
-  new Card('safe'),
-  new Card('safe'),
-  new Card('safe')
+    var hide = function(){
+      myDiv.style.display = "none";
+    };
+  })();
+
+  // bunch of cards in itx
+  $('.card').show();
+
+  var deck = [
+    new Card('stick'),
+    new Card('stick'),
+    new Card('stick'),
+    new Card('bomb'),
+    new Card('bomb'),
+    new Card('bomb'),
+    new Card('safe'),
+    new Card('safe'),
+    new Card('safe')
   ];
 
-deck = shuffle(deck);
+  deck = shuffle(deck);
   // target all cards $('.card')
 
   // loop thru each card and
-$('.back').each(function(index, el) {
-  var type = deck[index].type;
+  $('.back').each(function(index, el) {
+    var type = deck[index].type;
     // apply a card as a class
-  $(el).addClass(type);
-    // $('div').text('hi');
-    // console.log(deck);
-    // deck[index].value;
+    $(el).addClass(type);
   });
-};
+
+}; // start
 
 
 // stackoverflow: javascript button
@@ -148,33 +113,6 @@ var reset = function() {
   window.alert('resetted');
   location.reload();
 };
-
-
-// change something to reveal the card underneath
-// $( '.back' ).toggle(
-//   function() {
-//     $( this ).toggleClass( ".flip" );
-//   }, function() {
-//     $( this ).toggleClass( ".flip" );
-//   }
-// );
-
-// var flipCard = function() {
-
-// };
-// var strType = type;
-// strType = $( "p:first" ).text();
-// $( "p:last" ).html( str );
-
-// p.text(el)
-
-
-
-// var stickCount = function() {
-//   'stick'+
-// };
-
-
 
 
 //this shuffles cards
